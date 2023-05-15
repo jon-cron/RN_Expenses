1f55ca6edce552baf651478bb1017e634ed22931
jon-cron <jonathanacm95@gmail.com>
2023-05-15 11:50:17 -0600

    navigation installed

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
+// NOTE to setup navigation. install the npm and npx expo commands found in the docs. The individuall install the tabs and stack navigator found in the docs
+// NOTE step 1 : npx expo install react-native-screens react-native-safe-area-context
+// NOTE step 2: npm install react-native-screens react-native-safe-area-context
+// NOTE step 3: npm install @react-navigation/native-stack
+// NOTE step 4: npm install @react-navigation/bottom-tabs
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import RecentExpenses from "./screens/RecentExpenses";
