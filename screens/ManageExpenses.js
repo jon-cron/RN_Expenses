45279653df712e5ab6951b38673e57bd2a9de273
jon-cron <jonathanacm95@gmail.com>
2023-05-17 12:32:24 -0600

    push

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { View, Text, Image, Pressable, StyleSheet } from "react-native";
 
-const ManageExpenses = ({ route }) => {
+const ManageExpenses = ({ route, navigation }) => {
   // NOTE since we can land on this screen without params we must use params?. to avoid errors
   const expenseId = route.params?.expenseId;
   // NOTE the double !! turns whatever is attached to it a boolean. so if there is no expenseId it will read false; if there is an id it will read true
   const isEditting = !!expenseId;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditting ? "Edit Expense" : "Add Expense",
+    });
+  }, [isEditting, navigation]);
   return (
     <View>
       <Text>ManageExpenses</Text>
