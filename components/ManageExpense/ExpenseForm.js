3caf62785b5c6aeee90e4b007420a0591a67127d
jon-cron <jonathanacm95@gmail.com>
2023-05-19 11:20:32 -0600

    form made

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -10,15 +10,58 @@ import {
 import Input from "./Input";
 
 const ExpenseForm = () => {
+  const amountChangedHandler = () => {};
+
   return (
-    <View>
-      <Input label="Description" />
-      <Input label="Amount" />
-      <Input label="Date" />
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputRow}>
+        <Input
+          style={styles.rowInput}
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: amountChangedHandler,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: () => {},
+          }}
+        />
+      </View>
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    marginVertical: 10,
+  },
+});
 
 export default ExpenseForm;
--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,17 +1,43 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Global } from "../../constants/styles";
 
-const Input = ({ label, textInputConfig }) => {
+const Input = ({ label, textInputConfig, style }) => {
+  let inputStyles = [styles.input];
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={[styles.inputContainer, style]}>
+      <Text style={styles.label}>{label}</Text>
       {/* NOTE rather than setting a bunch of props to this input as seen below we can accept an entire config and spread that. */}
       {/* <TextInput keyboardType={type} /> */}
-      <TextInput {...textInputConfig} />
+      <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: Global.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: Global.colors.primary100,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+    color: Global.colors.primary700,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});
 
 export default Input;
