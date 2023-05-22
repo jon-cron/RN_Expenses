8b0325c2a5f873c434f170cea4ba3f4975d84402
jon-cron <jonathanacm95@gmail.com>
2023-05-22 11:07:55 -0600

    UI form handling

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Image, Pressable, StyleSheet } from "react-native";
+import { View, Text, Image, Pressable, StyleSheet, Alert } from "react-native";
 import Input from "./Input";
 import { Global } from "../../constants/styles";
 import Button from "../UI/Button";
@@ -31,6 +31,13 @@ const ExpenseForm = ({ isEditting, onCancel, onSubmit, defaultValues }) => {
       date: new Date(inputValues.date),
       description: inputValues.description,
     };
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== "Invalid Date";
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      Alert.alert("Invalid input", "please check your input values");
+      return;
+    }
     onSubmit(expenseData);
   };
   console.log(inputValues);
